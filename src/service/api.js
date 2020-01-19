9fe17613b76133e87c86430dc19c7dc6539ad087
grazidiandra <grazi.diandra@gmail.com>
2020-01-19 13:54:04 -0300

    refactor base url from service

--- src/service/api.js
+++ src/service/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import jsonpAdapter from 'axios-jsonp';
 
+const baseURL = 'https://viacep.com.br/ws/'
+
 const api = axios.create({
-    baseURL: 'https://viacep.com.br/ws/',
+    baseURL: baseURL,
     adapter: jsonpAdapter,
     callbackParamName: 'address'
 });
