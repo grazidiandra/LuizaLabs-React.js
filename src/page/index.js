ed67696d5cf62d171cd3b7fc4da96a8d725b86c0
grazidiandra <grazi.diandra@gmail.com>
2020-01-19 16:16:03 -0300

    added netlify configs to redirect

--- src/page/index.js
+++ src/page/index.js
@@ -33,7 +33,7 @@ class Cep extends Component {
         error: 'Preencha o campo de CEP '
       });
     } else {
-      api.get(`${this.state.cep}/json/?callback=address`)
+      api.get(`ws/${this.state.cep}/json/?callback=address`)
       .then(response => {
         if (response.data.erro) {
           this.setState({ 
