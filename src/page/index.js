7a2be74feb02069d6f8e85e137a0fa22ba960cab
grazidiandra <grazi.diandra@gmail.com>
2020-01-20 12:28:50 -0300

    move baseurl to service file

--- src/page/index.js
+++ src/page/index.js
@@ -34,7 +34,7 @@ class Cep extends Component {
         error: 'Preencha o campo de CEP '
       });
     } else {
-      api.get(`${this.state.cep}/json/?callback=address`)
+      api.get(`https://viacep.com.br/ws/${this.state.cep}/json/?callback=address`)
       .then(response => {
         if (response.data.erro) {
           this.setState({ 
